42ec3005110af44c16805d1e80160cbab27345cd
Arda Azim Taşözü <76783723+Ardaazimtsz@users.noreply.github.com>
2024-10-04 09:44:29 +0300

    Add files via upload

---
+++ Algoritmik Mülakat Sorusu/Algoritmik Mülakat Sorusu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmik_Mülakat_Sorusu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bakteri = 1;
+            for (int i = 1; i <= 24; i++)
+            {
+                bakteri = bakteri * 2;
+                Console.WriteLine(i + " Saat Sonuda: " + bakteri + " Oluşmuştur");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Aritmetik 4 İşlem Uygulaması/Aritmetik 4 İşlem Uygulaması/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2, toplam, fark, carpma, bolme;
+            Console.WriteLine("*****Aritemtik İşlemleri******* ");
+            Console.WriteLine(" ");
+            s1 = 20;
+            s2 = 30;
+            toplam = s1 + s2;
+            fark = s1 - s2;
+            carpma = s1 * s2;
+            bolme = s1 / s2;
+            Console.WriteLine("Toplam: "+ toplam);
+            Console.WriteLine("Farklar: "+ fark);
+            Console.WriteLine("Carpma: "+ carpma);
+            Console.WriteLine("Bolme: "+ bolme);
+            Console.WriteLine();
+            Console.Write("*****Aritemtik İşlemleri******* ");
+            Console.Read();
+
+        }
+    }
+}
